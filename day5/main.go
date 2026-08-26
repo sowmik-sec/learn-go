42fcca0d4c873352a869e1e2358cb4a0216203c7
sowmik-sec <sowmik.sec@gmail.com>
2026-08-26 11:43:25 +0600

    refactor: remove unused clamp function and update main to use maxOf

--- day5/main.go
+++ day5/main.go
@@ -17,18 +17,36 @@ import "fmt"
 // 	fmt.Println(half(3))
 // }
 
-func clamp(n, lo, hi int) (result int) {
-	if n < lo {
-		result = lo
-		return
-	} else if n > hi {
-		result = hi
-		return
+// func clamp(n, lo, hi int) (result int) {
+// 	if n < lo {
+// 		result = lo
+// 		return
+// 	} else if n > hi {
+// 		result = hi
+// 		return
+// 	}
+// 	return
+// }
+
+// func main() {
+// 	fmt.Println(clamp(15, 0, 10))
+// 	fmt.Println(clamp(-3, 0, 10))
+// }
+
+func maxOf(nums ...int) int {
+	max := -11111111
+	for _, val := range nums {
+		if val > max {
+			max = val
+		}
 	}
-	return
+	if max == -11111111 {
+		return 0
+	}
+	return max
 }
 
 func main() {
-	fmt.Println(clamp(15, 0, 10))
-	fmt.Println(clamp(-3, 0, 10))
+	nums := []int{4, 1, 9, 3}
+	fmt.Println(maxOf(nums...))
 }
